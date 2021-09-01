3b5f41f3bd62e7ea77841ab390c60b3f1d29aa22
Shinya Ishida <shinyaishida@users.noreply.github.com>
2021-09-01 19:45:37 +0900

    fix logger formatting

--- lib/mjai/logger.rb
+++ lib/mjai/logger.rb
@@ -6,7 +6,11 @@ module Mjai
   LOGGER = Logger.new($stdout)
   LOGGER.level = Logger::DEBUG
   LOGGER.formatter = proc do |severity, datetime, _progname, msg|
-    format("[%<date>s %<pid>d] %5<severity>s -- %<msg>s\n",
-           datetime.strftime('%Y-%m-%d %H:%M:%S'), Process.pid, severity, msg)
+    format("[%<date>s %<pid>d] %5<severity>s -- %<msg>s\n", {
+             date: datetime.strftime('%Y-%m-%d %H:%M:%S'),
+             pid: Process.pid,
+             severity: severity,
+             msg: msg
+           })
   end
 end
