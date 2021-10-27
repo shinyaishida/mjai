349278c79399be7a896afceccf3fab3d26d0a09f
Shinya Ishida <shinyaishida@users.noreply.github.com>
2021-10-27 20:25:35 +0900

    user name must be unique

--- mjai/app/models/user.rb
+++ mjai/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.name = name.downcase }
+  validates :name, presence: true,
+                   length: { minimum: 3, maximum: 12 },
+                   format: { with: /\A\w+\Z/i },
+                   uniqueness: { case_sensitive: false }
 end
--- mjai/db/schema.rb
+++ mjai/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_110025) do
+ActiveRecord::Schema.define(version: 2021_10_27_111251) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end
